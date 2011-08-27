88e56e541558bba10561bfd05b92fcd5b124d4c8
Chris Marrin <chris@marrin.com>
2011-08-26 22:13:40 -0700

    Changing Fraise to Strawberry

--- Command Line Utility/berry_main.m
+++ Command Line Utility/berry_main.m
@@ -1,15 +1,19 @@
 /*
-Fraise version 3.7 - Based on Smultron by Peter Borg
-Written by Jean-François Moy - jeanfrancois.moy@gmail.com
-Find the latest version at http://github.com/jfmoy/Fraise
+Strawberry - Based on Fraise by Jean-François Moy
+Written by Chris Marrin - chris@marrin.com
+Find the latest version at http://github.com/cmarrin/Strawberry
 
 Copyright 2010 Jean-François Moy
  
-Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file 
+except in compliance with the License. You may obtain a copy of the License at
  
 http://www.apache.org/licenses/LICENSE-2.0
  
-Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+Unless required by applicable law or agreed to in writing, software distributed under the 
+License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+either express or implied. See the License for the specific language governing permissions 
+and limitations under the License.
 */
 
 #import <Foundation/Foundation.h>
@@ -21,8 +25,8 @@ int main (int argc, const char * argv[]) {
 	NSFileManager *fileManager = [NSFileManager defaultManager];
 	NSWorkspace *workspace = [NSWorkspace sharedWorkspace];
 	if (!argv[1]) { // There is no argument
-		if (![workspace launchApplication:@"Fraise.app"]) {
-			NSLog(@"Can't open Fraise");
+		if (![workspace launchApplication:@"Strawberry.app"]) {
+			NSLog(@"Can't open Strawberry");
 		}
 	} else { // We should open files
 		short i = 1;
@@ -38,8 +42,8 @@ int main (int argc, const char * argv[]) {
 				[fileManager createFileAtPath:path contents:nil attributes:attributes];
 			}
 
-			if (![workspace openFile:path withApplication:@"Fraise.app"]) { // Open file
-				NSLog(@"Couldn't open %@ with Fraise", path);
+			if (![workspace openFile:path withApplication:@"Strawberry.app"]) { // Open file
+				NSLog(@"Couldn't open %@ with Strawberry", path);
 			}
 			i++;
 		}
