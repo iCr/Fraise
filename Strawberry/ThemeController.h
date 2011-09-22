8f4197cc96589ed1fcdbc758192275f337ddf191
Chris Marrin <cmarrin@apple.com>
2011-09-22 09:05:20 -0700

    Rearranging xib files. Moved existing ones and added ThemeController for new Themes pref panel

---
+++ Strawberry/ThemeController.h
@@ -0,0 +1,16 @@
+//
+//  ThemeController.h
+//  Strawberry
+//
+//  Created by Chris Marrin on 9/22/11.
+//  Copyright (c) 2011 Apple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ThemeController : NSObject
+{
+    IBOutlet NSTableView* m_table;
+}
+
+@end
