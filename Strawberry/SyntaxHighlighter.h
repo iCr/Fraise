4fd1bba898536d601f964e4cb866651b5eba67f3
Chris Marrin <chris@marrin.com>
2011-10-03 09:47:56 -0700

    WIP - Making highlighter use native classes for storage

---
+++ Strawberry/SyntaxHighlighter.h
@@ -0,0 +1,13 @@
+//
+//  SyntaxHighlighter.h
+//  Strawberry
+//
+//  Created by Chris Marrin on 10/2/11.
+//  Copyright (c) 2011 Apple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SyntaxHighlighter : NSObject
+
+@end
---
+++ Strawberry/SyntaxHighlighter.m
@@ -0,0 +1,13 @@
+//
+//  SyntaxHighlighter.m
+//  Strawberry
+//
+//  Created by Chris Marrin on 10/2/11.
+//  Copyright (c) 2011 Apple. All rights reserved.
+//
+
+#import "SyntaxHighlighter.h"
+
+@implementation SyntaxHighlighter
+
+@end
